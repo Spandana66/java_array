6b6fcb9bb36ce9c899772efaa865e982a964f278
Spandana66 <macharalaspandana1999@gmail.com>
2025-02-19 20:11:54 +0530

    Add files via upload

---
+++ Ascvalue_EvenorOdd.java
@@ -0,0 +1,17 @@
+class Ascvalue_EvenorOdd 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Program Start");
+		char a ='A';
+		if(a%2==0)
+		{
+		System.out.println(a + "is Even");
+	}
+	else
+	{
+	System.out.println(a + "is Odd");
+	}
+	System.out.println("Program Ends");
+	}
+	}
